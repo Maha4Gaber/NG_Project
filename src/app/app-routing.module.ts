b369e4c7f9a0817354c633f705216763e78ac9ab
Maha4Gaber <77496313+Maha4Gaber@users.noreply.github.com>
2024-05-16 21:50:27 +0300

    project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { MenuComponent } from './Components/menu/menu.component';
+import { GalleryComponent } from './Components/gallery/gallery.component';
+import { ReviewsComponent } from './Components/reviews/reviews.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { ErorrComponent } from './Components/erorr/erorr.component';
+import { LoginComponent } from './Components/login/login.component';
+import { AddMealComponent } from './Dashbourd/add-meal/add-meal.component';
+import { AddAdminComponent } from './Dashbourd/add-admin/add-admin.component';
+import { UpdateMealComponent } from './Dashbourd/update-meal/update-meal.component';
+import { MainComponent } from './Dashbourd/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {    path:'',component:HomeComponent  },
+  {    path:'about',component:AboutComponent  },
+  {    path:'menu',component:MenuComponent  },
+  {    path:'gallery',component:GalleryComponent  },
+  {    path:'reviews',component:ReviewsComponent  },
+  {    path:'contact',component:ContactComponent  },
+  {    path:'login',component:LoginComponent  },
+  {    path:'addmeal',component:AddMealComponent  },
+  {    path:'addadmin',component:AddAdminComponent  },
+  {    path:'updatemeal/:id',component:UpdateMealComponent},
+  {    path:'dashboard',component:MainComponent  },
+  {    path:'**',component:ErorrComponent  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,55 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { AboutComponent } from './Components/about/about.component';
+import { MenuComponent } from './Components/menu/menu.component';
+import { GalleryComponent } from './Components/gallery/gallery.component';
+import { ReviewsComponent } from './Components/reviews/reviews.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { MainComponent } from './Dashbourd/main/main.component';
+import { AddMealComponent } from './Dashbourd/add-meal/add-meal.component';
+import { AddAdminComponent } from './Dashbourd/add-admin/add-admin.component';
+import { UsersComponent } from './Dashbourd/users/users.component';
+import { OrdersComponent } from './Dashbourd/orders/orders.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { OrderComponent } from './Components/order/order.component';
+import { LoginComponent } from './Components/login/login.component';
+import { ErorrComponent } from './Components/erorr/erorr.component';
+import { UpdateMealComponent } from './Dashbourd/update-meal/update-meal.component';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    AboutComponent,
+    MenuComponent,
+    GalleryComponent,
+    ReviewsComponent,
+    ContactComponent,
+    MainComponent,
+    AddMealComponent,
+    AddAdminComponent,
+    UsersComponent,
+    OrdersComponent,
+    FooterComponent,
+    OrderComponent,
+    LoginComponent,
+    ErorrComponent,
+    UpdateMealComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterOutlet,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
