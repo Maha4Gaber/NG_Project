732341ecc7b227ea493a19ecb4b591b0f6293e2e
Rania Ahmed <raniaa7med2092017@gmail.com>
2024-05-19 14:54:46 +0300

    Rania

--- src/app/Components/menu/menu.component.ts
+++ src/app/Components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MealService } from 'src/app/Servecis/meal.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+ meals:any
+
+  constructor(private MealService: MealService ){
+  }
+  ngOnInit(): void {
+    this.MealService.GetAllMeals().subscribe({
+      next:(data)=>{
+        this.meals=data
+        // this._router.navigate(['/showMeals']) 
+        console.log(data);
+        
+      },
+      error:(err)=>{console.log(err)}
+    }) 
+  }
+
 
 }
