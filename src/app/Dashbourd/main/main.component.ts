e90df2b4e5884b45cf6defcd6fe8855889783ddf
Maha4Gaber <77496313+Maha4Gaber@users.noreply.github.com>
2024-05-20 22:48:17 +0300

    done

--- src/app/Dashbourd/main/main.component.ts
+++ src/app/Dashbourd/main/main.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
-
+export class MainComponent implements OnInit {
+  constructor( private router: Router) {}
+    ngOnInit(): void {
+    if(localStorage.getItem('userrole')!='admin')
+      {
+        this.router.navigate(['/login']);
+      }
+    
+  }
 }
